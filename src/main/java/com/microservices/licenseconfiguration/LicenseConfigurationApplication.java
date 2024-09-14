a4561d178b78616b16beac3e5dd4745c23f0f984
gaju-01 <you@example.com>
2024-09-14 14:47:02 +0530

    First commit

---
+++ src/main/java/com/microservices/licenseconfiguration/LicenseConfigurationApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.licenseconfiguration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicenseConfigurationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LicenseConfigurationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/microservices/licenseconfiguration/LicenseConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.licenseconfiguration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LicenseConfigurationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
